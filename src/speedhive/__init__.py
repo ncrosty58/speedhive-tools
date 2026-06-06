ca3bd472e2b085992aa06fe376aa0e88a1720c5a
Nathan Crosty <nathancrosty@gmail.com>
2026-06-05 21:44:02 -0400

    chore: release v0.2.1: Python 3.12 compatibility and CLI subparser fixes

--- src/speedhive/__init__.py
+++ src/speedhive/__init__.py
@@ -5,4 +5,4 @@ from speedhive.wrapper import SpeedhiveClient
 from speedhive.core import File, Response, UNSET, Unset
 from speedhive.errors import UnexpectedStatus
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
