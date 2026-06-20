f7cff18528a41a65722690808f5f6403e9391d8b
Nathan Crosty <nathancrosty@gmail.com>
2026-06-19 22:28:07 -0400

    Bump version to 0.7.0

--- src/speedhive/__init__.py
+++ src/speedhive/__init__.py
@@ -5,4 +5,4 @@ from speedhive.wrapper import SpeedhiveClient
 from speedhive.core import File, Response, UNSET, Unset
 from speedhive.errors import UnexpectedStatus
 
-__version__ = "0.6.2"
+__version__ = "0.7.0"
